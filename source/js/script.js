cb8fe5047fa51c9a64964d2c5c6fe1f485677f76
Dmitry <kamaelix69@gmail.com>
2022-01-04 01:40:08 +0300

    декоративные элементы и стилизация главной страницы

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+var navBB = document.querySelector(".main-nav");
+console.log(navBB);
+var navToggle = document.querySelector(".main-nav__toggler");
+
+navBB.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function () {
+  if (navBB.classList.contains("main-nav--closed")) {
+    navBB.classList.remove("main-nav--closed");
+    navBB.classList.add("main-nav--opened");
+  } else {
+    navBB.classList.add("main-nav--closed");
+    navBB.classList.remove("main-nav--opened");
+  }
+});
